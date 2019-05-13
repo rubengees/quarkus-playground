75168fb203701e1b456374d9a374f3d86cdf7858
Ruben Gees <rubengees7@googlemail.com>
2019-05-13 23:26:13 +0200

    Extend test

--- src/test/java/com/rubengees/playground/TodoResourceTest.java
+++ src/test/java/com/rubengees/playground/TodoResourceTest.java
@@ -1,9 +1,11 @@
 package com.rubengees.playground;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.mapper.ObjectMapperType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
 
 @QuarkusTest
 public class TodoResourceTest {
@@ -11,8 +13,16 @@ public class TodoResourceTest {
     @Test
     public void testTodosEndpoint() {
         given()
+                .header("Content-Type", "application/json")
+                .body(new Todo("Test", "description", true), ObjectMapperType.JACKSON_2)
+                .when().post("/todos")
+                .then()
+                .statusCode(204);
+
+        given()
                 .when().get("/todos")
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body("", hasSize(1));
     }
 }
